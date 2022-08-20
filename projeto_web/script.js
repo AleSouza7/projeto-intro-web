8cf665f9276f20982f5ef45c355b45aa4fdfac31
Alesouza7 <alexcostasouza86@yahoo.com>
2022-08-19 21:44:46 -0300

    Semana2

--- projeto_web/script.js
+++ projeto_web/script.js
@@ -1,14 +1,14 @@
 //semana1 requisito 1 criar tres variaveis, string, number e boolean
 //requisito 2 criar 3 conjuntos de variaveis
-const nome1 = "Alex"//string
+const nome1 = ("Alex").toUpperCase()//string
 const idade1 = 20 //number
 const temCadastro1 = true //boolean
 
-const nome2 = "Maria"//string
+const nome2 = ("Maria").toUpperCase()//string
 const idade2 = 40 //number
 const temCadastro2 = false //boolean
 
-const nome3 = "Alberi"//string
+const nome3 = ("Alberi").toUpperCase()//string
 const idade3 = 50 //number
 const temCadastro3 = true //boolean
 
@@ -22,3 +22,14 @@ const checkCadastro = temCadastro1 && temCadastro2 && temCadastro3
 
 console.log(media)//debug de verificação do requisito 3
 console.log(checkCadastro)//debug de verificação do requisito 4
+
+//Semana 2
+//Criar mais 1 elemento que seja um array e que mantenha o tipo de dado string boolean ou number
+ const cidade = ["Porto Alegre","São Paulo","Sergipe","Bahia"]
+
+ //Mostrar tudo o que foi criado
+ //titulo ou neste caso trazer a string nome sempre em maiusculo
+ console.log("Nomes:"+nome1,nome2,nome3)
+ console.log("Idades:"+idade1,idade2,idade3)
+ console.log("Cadastros:"+ temCadastro1,temCadastro2,temCadastro3)
+ console.log(`Array de cidades: ${cidade}`)
