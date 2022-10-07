33faee4d550cee97afc2e7ab5b15c79101526b17
Alesouza7 <alexcostasouza86@yahoo.com>
2022-10-07 16:36:54 -0300

    Semana7

--- projeto_web/script.js
+++ projeto_web/script.js
@@ -14,7 +14,7 @@ const temCadastro3 = true //boolean
 
 //requisito 3 fazer a media das tres idades
 
-const media = Number((idade1 + idade2 + idade3)/3).toFixed(2)//Acrescentei o toFixed para limitar as casas decimais caso numero seja fracionado
+const media = Number((idade1 + idade2 + idade3) / 3).toFixed(2)//Acrescentei o toFixed para limitar as casas decimais caso numero seja fracionado
 
 //requisito 4 imprimir a verificação de booleans
 
@@ -26,33 +26,33 @@ console.log(checkCadastro)//debug de verificação do requisito 4
 
 //Semana 2
 //Criar mais 1 elemento que seja um array e que mantenha o tipo de dado string boolean ou number
- const cidade = ["Porto Alegre","São Paulo","Sergipe","Bahia"]
+const cidade = ["Porto Alegre", "São Paulo", "Sergipe", "Bahia"]
 
- //Mostrar tudo o que foi criado
- //titulo ou neste caso trazer a string nome sempre em maiusculo
- console.log("Semana 2")
- console.log("Nomes:"+nome1,nome2,nome3)
- console.log("Idades:"+idade1,idade2,idade3)
- console.log("Cadastros:"+ temCadastro1,temCadastro2,temCadastro3)
- console.log(`Array de cidades: ${cidade}`)
+//Mostrar tudo o que foi criado
+//titulo ou neste caso trazer a string nome sempre em maiusculo
+console.log("Semana 2")
+console.log("Nomes:" + nome1, nome2, nome3)
+console.log("Idades:" + idade1, idade2, idade3)
+console.log("Cadastros:" + temCadastro1, temCadastro2, temCadastro3)
+console.log(`Array de cidades: ${cidade}`)
 
- //semana 3
- //Requisito 1. Transforme os itens que criamos nas últimas semanas em objetos.
+//semana 3
+//Requisito 1. Transforme os itens que criamos nas últimas semanas em objetos.
 
- const objeto1= {nome1,idade1,temCadastro1}
- const objeto2= {nome2,idade2,temCadastro2}
- const objeto3= {nome3,idade3,temCadastro3}
+const objeto1 = { nome1, idade1, temCadastro1 }
+const objeto2 = { nome2, idade2, temCadastro2 }
+const objeto3 = { nome3, idade3, temCadastro3 }
 
- console.log("Semana 3")//Semana 3
- console.log(objeto1,objeto2,objeto3)
+console.log("Semana 3")//Semana 3
+console.log(objeto1, objeto2, objeto3)
 
- //requisito 2. Crie um array para guardar os objetos. Este array deve estar vazio
+//requisito 2. Crie um array para guardar os objetos. Este array deve estar vazio
 
- const arrayObjeto=[]
+const arrayObjeto = []
 
 //requisito 3. Adicione os objetos criados no item 1 ao array de objetos criado no item 2, utilizando o push()
 
-arrayObjeto.push(objeto1,objeto2,objeto3)
+arrayObjeto.push(objeto1, objeto2, objeto3)
 
 console.log(arrayObjeto)
 
@@ -63,15 +63,32 @@ console.log("Semana 4")
 //Requisito 1 Altere o objeto da semana 4
 const novoObjeto = {//criado um objeto para adicionar ao array arrayObjeto
     nome4: "Vitória",
-    idade4: 5 ,
+    idade4: 5,
     temCadastro4: false
 }
 
-if(arrayObjeto == true){
-    console.log("validado")
-    arrayObjeto.push(novoObjeto)
-}else{
-    alert("O item não foi adicionado")
-}
-console.log(arrayObjeto)
+// if (arrayObjeto == true) {
+//     console.log("validado")
+//     arrayObjeto.push(novoObjeto)
+// } else {
+//     alert("O item não foi adicionado")
+// }
+// console.log(arrayObjeto)
 
+console.log("Semana 5")//Semana 5
+
+
+console.log("Semana 6")//Semana 6
+
+
+//Refatoração do projeto // semana 1
+
+const nomeAries = "Mu";
+const idadeAries = 20;
+const vivoAries = true;
+const poderAries = ["Muralha de Cristal","Revolução Estelar","Extinção Estelar","Telecinese","Rede de Cristal"];
+
+const ouroAries=[]
+ouroAries.push(nomeAries,idadeAries,vivoAries,poderAries)
+
+console.log(ouroAries)
